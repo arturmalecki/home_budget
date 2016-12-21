3d2abe3646a88e101e311931d5ac3f91cc4a851b
Artur Malecki <a.malecki@livelinktechnology.net>
2016-12-21 15:45:44 +0000

    Add some configuration to guardian

--- config/test.exs
+++ config/test.exs
@@ -17,3 +17,16 @@ config :home_budget, HomeBudget.Repo,
   database: "home_budget_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
+
+config :guardian, Guardian,
+  allowed_algos: ["ES512"],
+  issuer: "HomeBudget",
+  ttl: { 30, :days },
+  serializer: HomeBudget.GuardianSerializer,
+  secret_key: %{
+    "crv" => "P-521",
+    "d" => "t448_42yrdbLASkHUvgPH0g8lvCCVmgKdTYeUODr6fI3rDSs41B6jmBhCyjcgnho9sOXgMOfSgRfmqyOCQuW4Kw",
+    "kty" => "EC",
+    "x" => "ANTQQSkUGOUHmTKe_9-MgDisqdcz3fBPMUeYUmtDhRsdg5NQmHQBbnWfMPB3Px4l4DIJY8bWRVejv6Z2yclpg80m",
+    "y" => "AET6RGnO4Tqj4V8NMMtT7fpRWQ68x4rmxQkLO5-jUTsEbWcgeW_SFlbM9JE2jLujjHSqTDDboAbD2SYDo1XzAzBM"
+  }
